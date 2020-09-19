db56937d7ffcf9e4a9613e11f77564c15aa41d36
alexey gyori <alexey.gyoeri@gmail.com>
2020-09-19 15:27:37 +0200

    - made API return actual results - WIP rule API

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,9 @@ from flask import Flask, request
 import os.path
 
 import utils
-from data_model.team import all_teams
+from data_model.team import get_team
+from data_model.user import get_user
+
 
 app = Flask(__name__)
 
@@ -17,37 +19,71 @@ def hello_world():
 
 @app.route('/team')
 def get_team():
+    # results maps user-> scores, favorable users (with score better than 0)
     if 'team_id' not in request.args:
-        return 'Team id is missing in the request.', 400
+        return 'team_id is missing in the request.', 400
+    team_id = request.args['team_id']
 
-    team_id = request.args['team_id']  # TODO: support getting the real data
-    with open(os.path.join('example_json', 'team.json'), 'r') as f:
-        return f.read().replace('"team_id": 1', f'"team_id": {team_id}')
+    try:
+        team = get_team(team_id)
+    except Exception as e:
+        return f'invalid team id {e}', 400
+
+    return json.dumps(team, default=utils.default_serialize)
 
 
 @app.route('/user')
-def get_user():
+def user_route():
     if 'user_id' not in request.args:
         return 'user_id is missing in the request.', 400
-    user_id = request.args['user_id']  # TODO: support getting the real data
-    with open(os.path.join('example_json', 'user.json'), 'r') as f:
-        return f.read().replace('"user_id": 1', f'"user_id": {user_id}')
+    user_id = request.args['user_id']
+    try:
+        user = get_user(user_id)
+    except Exception as e:
+        return f'invalid user id {e}', 400
+
+    return json.dumps(user, default=utils.default_serialize)
 
 
 @app.route('/results')
 def get_results():
-    # TODO: error handling
-    # results maps user-> scores, favorable users (with score better than 0)
-    team_id = request.args['team_id']
-
-    if team_id not in all_teams:
-        return "invalid team id", 400
-    return json.dumps(all_teams[team_id], default=utils.default_serialize)
+    return "to be implemented"
 
 
 @app.route('/rule', methods=['POST'])
 def post_rule():
-    pass
+    if 'user_id' not in request.args:
+        return 'user_id is missing in the request.', 400
+    user_id = request.args['user_id']
+
+    if 'day' not in request.args:
+        return 'day is missing in the request.', 400
+    day = request.args['day']
+
+    if 'preference' not in request.args:
+        return 'preference is missing in the request.', 400
+    preference = request.args['preference']
+
+    try:
+        user = get_user(user_id)
+    except Exception as e:
+        return f'invalid user id {e}', 400
+
+    if day == 0:
+        user.monday = preference
+    elif day == 1:
+        user.tuesday = preference
+    elif day == 2:
+        user.wednesday = preference
+    elif day == 3:
+        user.thursday = preference
+    elif day == 4:
+        user.friday = preference
+    else:
+        return "invalid day id, must be between 0 and 4", 400
+
+    return "successfully posted the rule", 200
+
 
 
 if __name__ == '__main__':
---
+++ backend/tests/test_post_rule.sh
@@ -0,0 +1,1 @@
+curl --data "user_id=1&day=2&preference=3" http://localhost:5000/rule
